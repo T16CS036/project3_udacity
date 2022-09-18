11f5a41c43608c651cac44192351cd197ba9eb4e
MinhDTH <dinhthaihoangminh@gmail.com>
2022-09-18 21:57:49 +0900

    partII

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+setx POSTGRES_USERNAME project3
+setx POSTGRES_PASSWORD 272309578712
+setx POSTGRES_HOST database-1.cd5amzkcep8i.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB database-1
+setx AWS_BUCKET preject3-272309578712-dev
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET testing
+setx URL http://localhost:8100
